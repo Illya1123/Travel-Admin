a02852103f87ce726418a987f196d31a17e20f43
Illya1123 <21520565@gm.uit.edu.vn>
2025-07-20 23:32:15 +0700

    sửa lỗi Sidebar bị biến mất khi chọn các tab khác

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Sidebar from './components/Sidebar'
+import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Users from './pages/Users'
 import Settings from './pages/Settings'
-import Login from './pages/Login'
 import ProtectedRoute from './components/ProtectedRoute'
 import PublicRoute from './components/PublicRoute'
+import AdminLayout from './components/AdminLayout'
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Chỉ vào được login nếu chưa đăng nhập */}
+        {/* Public route: login */}
         <Route
           path="/login"
           element={
@@ -21,24 +21,19 @@ function App() {
           }
         />
 
-        {/* Các route nội bộ yêu cầu đăng nhập */}
+        {/* Protected layout with nested routes */}
         <Route
           path="/"
           element={
             <ProtectedRoute>
-              <div className="flex">
-                <Sidebar />
-                <div className="flex-1 p-6 bg-gray-100 min-h-screen">
-                  <Routes>
-                    <Route path="/" element={<Dashboard />} />
-                    <Route path="/users" element={<Users />} />
-                    <Route path="/settings" element={<Settings />} />
-                  </Routes>
-                </div>
-              </div>
+              <AdminLayout />
             </ProtectedRoute>
           }
-        />
+        >
+          <Route index element={<Dashboard />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
       </Routes>
     </Router>
   )
---
+++ src/components/AdminLayout.jsx
@@ -0,0 +1,15 @@
+import Sidebar from './Sidebar'
+import { Outlet } from 'react-router-dom'
+
+const AdminLayout = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1 p-6 bg-gray-100 min-h-screen">
+        <Outlet />
+      </div>
+    </div>
+  )
+}
+
+export default AdminLayout
